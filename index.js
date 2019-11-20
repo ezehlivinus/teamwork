528cead83accfb0a747c4ca2dfca61287b19eab3
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2019-11-20 14:39:06 +0100

    added sync and async error handler middleware

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('express-async-errors');
 const express = require('express');
+const error = require('./middleware/error');
 
 const app = express();
+
+app.use(error);
 
 let port = process.env.port || 3000;
 const server = app.listen(port, () => console.log(`Listining on port ${port}`));
