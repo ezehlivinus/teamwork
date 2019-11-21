8b76f19b585e525b725b1e2667a64b8c90d8409b
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2019-11-21 13:08:09 +0100

    Added PostgreSQL db

---
+++ startup/config.js
@@ -0,0 +1,7 @@
+const config = require('config');
+
+module.exports = function() {
+  // if (!config.get('jwt_key')) {
+  //   throw new Error('ERRO: Json Web Token not defined');
+  // }
+};
---
+++ startup/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+const config = require('config');
+const logger = require('./logging')();
+
+const connection = config.get('db');
+const pool = new Pool(connection);
+
+pool.connect().then(() => logger.info('Connected to postgres db...'));
+
+module.exports = {
+  query: (queryString, queryParams) => pool.query(queryString, queryParams),
+  pool
+};
